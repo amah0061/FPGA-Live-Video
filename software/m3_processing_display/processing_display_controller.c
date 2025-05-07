2e2323b8c59573be80c4e85a15ec32fe60b56332
James Thomson <jtho0098@student.monash.edu>
2025-05-07 14:02:07 +1000

    setup projects for two processors in NIOS

---
+++ software/m3_processing_display/processing_display_controller.c
@@ -0,0 +1,26 @@
+/* This file alters the image received from the camera in a number of different ways depending
+ * on user requirements. The file then outputs the relevant image/images to an external
+ * monitor. The file also contains the calculations to generate the frames per second (FPS) of
+ * the system.
+ */
+/*
+* Created By :
+* Aadi Mahajan 33855994
+* James Thomson 33856257
+* Lance Miranda 31481795
+* Ryan Shanta 32284470
+* Xavier Hasiotis-Welsh 33880271
+*/
+// Last edited: 07/05/2025
+// version ='3.0'
+#include "sys/alt_stdio.h"
+
+int main()
+{ 
+  alt_putstr("Hello from Nios II p_processing_display!\n");
+
+  /* Event loop never exits. */
+  while (1);
+
+  return 0;
+}
